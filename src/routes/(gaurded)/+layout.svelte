74d3a0203642e504c2f628cb1d3f6341c84eff57
Aritra Mondal <aritra041999.dev@gmail.com>
2024-11-25 22:07:44 +0100

    feat: initial commit

---
+++ src/routes/(gaurded)/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import AppSidebar from '$lib/components/ui/app-sidebar/app-sidebar.svelte';
+	let { children } = $props();
+</script>
+
+<Sidebar.Provider>
+	<AppSidebar />
+	<main>
+		<Sidebar.Trigger />
+		{@render children()}
+	</main>
+</Sidebar.Provider>
