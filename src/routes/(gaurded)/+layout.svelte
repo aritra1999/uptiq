1f97d64b9f62e248f168a262c542828d88314675
Aritra Mondal <aritra041999.dev@gmail.com>
2024-11-27 22:32:51 +0100

    feat: update gaurded layout

--- src/routes/(gaurded)/+layout.svelte
+++ src/routes/(gaurded)/+layout.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import AppSidebar from '$lib/components/ui/app-sidebar/app-sidebar.svelte';
+
 	let { children } = $props();
 </script>
 
 <Sidebar.Provider>
 	<AppSidebar />
-	<main>
+	<main class="w-full">
 		<Sidebar.Trigger />
-		{@render children()}
+		<section class="container mx-auto">
+			{@render children()}
+		</section>
 	</main>
 </Sidebar.Provider>
