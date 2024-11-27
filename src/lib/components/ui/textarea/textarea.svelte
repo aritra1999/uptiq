b70a648da8e5ff3289c334d6b1a12b4fd2ed044a
Aritra Mondal <aritra041999.dev@gmail.com>
2024-11-27 22:32:20 +0100

    shadcn: add textarea

---
+++ src/lib/components/ui/textarea/textarea.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import type { WithElementRef, WithoutChildren } from 'bits-ui';
+	import type { HTMLTextareaAttributes } from 'svelte/elements';
+	import { cn } from '$lib/utils.js';
+
+	let {
+		ref = $bindable(null),
+		label,
+		value = $bindable(),
+		class: className,
+		...restProps
+	}: WithoutChildren<WithElementRef<HTMLTextareaAttributes & { label: string }>> = $props();
+</script>
+
+<div
+	class="w-full rounded-md border border-input bg-transparent px-4 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+>
+	<div class="mb-0.5 text-xs font-semibold text-muted-foreground">
+		{label}
+		<span class="text-red-500">
+			{#if restProps['required']}
+				*
+			{/if}
+		</span>
+	</div>
+	<textarea
+		class={cn('min-h-24  w-full bg-transparent text-sm outline-none ring-0', className)}
+		bind:value
+		{...restProps}
+	></textarea>
+</div>
