0e62def94c3c5c5f9d61cd9b49ec73cf58307fcf
Aritra Mondal <aritra041999.dev@gmail.com>
2024-11-27 23:08:10 +0100

    feat: add date and sleep utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,30 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export const prettifyDate = (date: Date): string => {
+	const timeFormatter = new Intl.DateTimeFormat('en-US', {
+		hour: '2-digit',
+		minute: '2-digit',
+		hour12: false
+	});
+
+	const dateFormatter = new Intl.DateTimeFormat('en-US', {
+		day: '2-digit',
+		month: 'short'
+	});
+
+	const time = timeFormatter.format(date);
+	const formattedDate = dateFormatter.format(date);
+
+	return `${time}, ${formattedDate}`;
+};
+
+export const sleep = (ms: number): Promise<void> => {
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms);
+	});
+};
