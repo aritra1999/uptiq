949f059410f17900f632dd50da61ffad89f33e68
Aritra Mondal <aritra041999.dev@gmail.com>
2024-11-28 21:49:03 +0100

    feat: add project details page

--- src/routes/(gaurded)/+layout.svelte
+++ src/routes/(gaurded)/+layout.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import AppSidebar from '$lib/components/ui/app-sidebar/app-sidebar.svelte';
+	import { projectStore } from '$lib/store/project.store';
 
-	let { children } = $props();
+	let { data, children } = $props();
+
+	$effect(() => {
+		const projects = data.projects;
+		if (projects) projectStore.set(projects);
+	});
 </script>
 
 <Sidebar.Provider>
