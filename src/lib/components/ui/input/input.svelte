c9e32569aafadabcf42da66daf7d24ffb804f2f6
Aritra Mondal <aritra041999.dev@gmail.com>
2024-12-05 23:31:46 +0100

    shadcn: update input (add input type time)

--- src/lib/components/ui/input/input.svelte
+++ src/lib/components/ui/input/input.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { HTMLInputAttributes } from 'svelte/elements';
+	import Clock from 'lucide-svelte/icons/clock';
 	import type { WithElementRef } from 'bits-ui';
 	import { cn } from '$lib/utils.js';
 
@@ -13,18 +14,31 @@
 </script>
 
 <div
-	class="w-full rounded-lg border border-input bg-transparent px-4 py-2 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+	class="flex w-full items-center justify-between rounded-lg border border-input bg-transparent px-4 py-2 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
 >
-	<div class="text-xs font-semibold text-muted-foreground">
-		{label}
-		<span class="text-red-500">
-			{#if restProps['required']}*{/if}
-		</span>
+	<div class="w-full">
+		<div class="text-xs font-semibold text-muted-foreground">
+			{label}
+			<span class="text-red-500">
+				{#if restProps['required']}*{/if}
+			</span>
+		</div>
+		<input
+			bind:this={ref}
+			class={cn('w-full bg-transparent text-sm outline-none ring-0', className)}
+			bind:value
+			{...restProps}
+		/>
 	</div>
-	<input
-		bind:this={ref}
-		class={cn('w-full bg-transparent text-sm outline-none ring-0', className)}
-		bind:value
-		{...restProps}
-	/>
+	{#if restProps['type'] === 'time'}
+		<div class="size-4">
+			<Clock class="size-4" />
+		</div>
+	{/if}
 </div>
+
+<style lang="pcss">
+	input::-webkit-calendar-picker-indicator {
+		display: none;
+	}
+</style>
