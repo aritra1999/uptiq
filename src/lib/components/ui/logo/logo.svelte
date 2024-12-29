ca56f449b7ec4bcbfae9c2de106358410e1ca2eb
Aritra Mondal <aritra1999.dev@gmail.com>
2024-12-30 00:16:11 +0100

    feat: make logo a link

--- src/lib/components/ui/logo/logo.svelte
+++ src/lib/components/ui/logo/logo.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
 	import { Tally4 } from 'lucide-svelte';
+	import { cn } from '$lib/utils';
+
+	let { variant = 'dark' } = $props();
 </script>
 
-<div
-	class="flex h-12 w-fit items-center rounded-full border bg-background px-6 text-lg font-semibold"
+<a
+	href="/"
+	class={cn(
+		'flex h-12 w-fit items-center rounded-full border px-6 text-lg font-semibold',
+		variant === 'dark' ? 'bg-background text-foreground' : 'bg-foreground text-background'
+	)}
 >
 	<Tally4 strokeWidth="3" class="mr-3 h-6 w-6 text-indigo-700" />
 	<span class="pb-1">uptiq</span>
-</div>
+</a>
